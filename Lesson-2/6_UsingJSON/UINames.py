d2750f79eecd7957445ff6f1d0a64c736a23f900
Amber Staines <amber.i.staines@gmail.com>
2018-10-16 15:49:01 -0500

    Finishing Lesson 2.

--- Lesson-2/6_UsingJSON/UINames.py
+++ Lesson-2/6_UsingJSON/UINames.py
@@ -15,10 +15,11 @@ def SampleRecord():
     r = requests.get("http://uinames.com/api?ext&region=United%20States",
                      timeout=2.0)
     # 1. Add a line of code here to decode JSON from the response.
+    first_name = r.json()['name']
+    last_name = r.json()['surname']
+    pin = r.json()['credit_card']['pin']
 
-    return "My name is {} {} and the PIN on my card is {}.".format(
-        # 2. Add the correct fields from the JSON data structure.
-    )
+    return "My name is {} {} and the PIN on my card is {}.".format(first_name, last_name, pin)
 
 if __name__ == '__main__':
     print(SampleRecord())
